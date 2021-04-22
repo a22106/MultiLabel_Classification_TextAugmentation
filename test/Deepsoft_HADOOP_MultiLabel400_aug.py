ea32607d322927e025fc5341036ea829e53930f8
PiusHwang <bk22106@gmail.com>
2021-04-22 20:55:45 +0900

    Update Deepsoft_HADOOP_MultiLabel400_aug.py

--- test/Deepsoft_HADOOP_MultiLabel400_aug.py
+++ test/Deepsoft_HADOOP_MultiLabel400_aug.py
@@ -43,8 +43,9 @@ class ML_Classification:
 #       self.data_location = '/home/a22106/python_practice/component_classification/DeepSoft-C/Dataset/Data/{}.csv'.format(self.dataset_name)
         
         # 데이터 위치
-        self.data_location_aug = 'HADOOP_aug.csv'
-        self.data_location_ori = 'HADOOP.csv'
+        self.data_location_aug = 'D:\\OneDrive\\GitHub\\BugTriage_a22106\\a22106\Dataset\\HADOOP_aug_mul3.csv'
+        self.data_location_ori = 'D:\\OneDrive\\GitHub\\BugTriage_a22106\\a22106\Dataset\\HADOOP.csv'
+        self.aug_mul = 0
 
         # 데이터 변수 입력
         self.data = pd.read_csv(self.data_location_aug) # 증강 데이터
@@ -53,27 +54,31 @@ class ML_Classification:
 
 
     def refine_origin_data(self):
-        data = self.data_ori
+        data_ori = self.data_ori
         
-        data_onehot = data.drop(columns = ['issuekey', 'title', 'description', 'component'])
+        data_onehot = data_ori.drop(columns = ['issuekey', 'title', 'description', 'component'])
         data_label = []
         for i in range(len(data_onehot)):
             data_label.append(list(data_onehot.iloc[i]))
 
         # make 'data' value
-        data_text = pd.Series(list(data["title"] + ' ' + data['description']), index = data.index)
+        data_text = pd.Series(list(data_ori["title"] + ' ' + data_ori['description']), index = data_ori.index)
 
         #data = data.drop(columns = ['issuekey', 'title', 'description', 'component'])
-        self.data_ori = pd.DataFrame(data = {'text': data_text, 'labels': data_label})
+        data_ori = pd.DataFrame(data = {'text': data_text, 'labels': data_label})
+        self.data_ori = data_ori
 
         # 오리지날 데이터를 train, eval데이터로 분리
-        self.train_data_ori, self.eval_data_ori = train_test_split(self.data_ori, test_size = 0.3)
+        self.train_data_ori, self.eval_data_ori = train_test_split(data_ori, test_size = 0.3)
         self.eval_data = self.eval_data_ori
+        self.train_data = self.train_data_ori
     
 
     # 불러온 정제된 데이터 one hot을 str에서 list로 바꾸는 작업
-    def labels_to_int(self):
-        data = self.data
+    def labels_to_int(self, aug_mul):
+        data = self.data[: 6152 * aug_mul] 
+        self.aug_mul = aug_mul
+
         changeChar = ' [],'
         for i in range(len(data)):
             for chanChar in changeChar:
@@ -82,29 +87,22 @@ class ML_Classification:
             data['labels'][i] = list(map(int, data['labels'][i]))
         
         # 증강데이터의 train_data에서 evaluation부분 제거
-        
         eval_index_list = list(self.eval_data.index)
         
-        for aug_num in range(4):
+        for aug_num in range(aug_mul):
             iidf2 = [i + 6152* aug_num for i in eval_index_list]
             self.eval_index = self.eval_index + iidf2
+
+        self.train_data = data.drop(self.eval_index)
+
         data = self.data
-
-# train, evaluation 데이터로 나누기 // 사용 안함
-    def split_data_eval(self, random_state = None):
-        data = self.data
-        train_data_ori, eval_data_ori = train_test_split(self.data_ori, test_size = 0.3, random_state = random_state)
-
-        eval_data_indexList = eval_data_ori.index.append(eval_data_ori.index * 2)
-        data = data.drop(eval_data_indexList)
-        
-        self.train_data, self.eval_data = train_test_split(data, test_size = 0.3, random_state = random_state)
-
 
 # 모델 parameter 설정
-    def set_model(self):
+    def set_model(self, nlp_model):
+        self.nlp_model_name = nlp_model
         self.model = MultiLabelClassificationModel(self.nlp_model_name, self.nlp_model[self.nlp_model_name], num_labels = self.labels_num, 
-        args = {'output_dir': '/data/a22106/Classify_/Outputs/HADOOP/{}/outputs20/'.format(self.nlp_model_name), 'overwrite_output_dir': False, 'num_train_epochs':400, 'gradient_accumulation_steps': 2, 'max_seq_length': 128, 'learning_rate': 5e-5})
+        args = {'output_dir': '/data/a22106/Classify_/Outputs/HADOOP/{}/outputs_char_keyboard_aug{}/'.format(self.nlp_model_name, self.aug_mul), 
+        'overwrite_output_dir': False, 'num_train_epochs':100, 'batch_size': 32, 'max_seq_length': 128, 'learning_rate': 5e-5})
 
     def train_model(self):
         self.model.train_model(self.train_data)
@@ -115,10 +113,17 @@ class ML_Classification:
     #def predict(self):
     #    self.preds, self.outputs = self.model.predict(self.test_data)
 
+# 원본 데이터 multilabel 학습
 ml = ML_Classification()
-
 ml.refine_origin_data()
-ml.labels_to_int()
-ml.set_model()
-#ml.train_model()
-#ml.eval_model()
+ml.set_model('bert')
+ml.train_model()
+ml.eval_model()
+
+# 2배 augmentation 데이터 multilabel 학습
+ml2 = ML_Classification()
+ml2.refine_origin_data()
+ml2.labels_to_int(2)
+ml2.set_model('bert')
+ml2.train_model()
+ml2.eval_model()
